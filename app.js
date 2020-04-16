83d3d5e73cf24fd4655bbb77411b566942aa7e00
Laitinen Petri <LaitinenP@karelia.fi>
2020-04-16 17:49:02 +0300

    app.js lisätty server listen

--- app.js
+++ app.js
@@ -42,4 +42,18 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.set('port', process.env.PORT || 3000);
+
+exports.listen = function () {
+    server = app.listen(app.get('port'), function () {
+        debug('Express server listening on port ' + server.address().port);
+    });
+}
+
+exports.close = function () {
+    server.close(() => {
+        debug('Server stopped.');
+    });
+}
+
 module.exports = app;
