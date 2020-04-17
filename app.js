7c6e2baa3ef3f0d0551852a1c93fd2b20a540506
Laitinen Petri <LaitinenP@karelia.fi>
2020-04-17 12:03:15 +0300

    Bin-hakemisto otettu mukaan

--- app.js
+++ app.js
@@ -42,18 +42,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.set('port', process.env.PORT || 3000);
-
-exports.listen = function () {
-    server = app.listen(app.get('port'), function () {
-        debug('Express server listening on port ' + server.address().port);
-    });
-}
-
-exports.close = function () {
-    server.close(() => {
-        debug('Server stopped.');
-    });
-}
-
 module.exports = app;
